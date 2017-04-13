a2b9771c477e9a6960d56d298ccedb75212ce0a8
wang.xur <wang.xur@husky.neu.edu>
2017-04-13 19:42:49 -0400

    Initial Commit

---
+++ SleepCycleAlarm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SleepCycleAlarm
+//
+//  Created by Mockin_Aeon. on 4/13/17.
+//  Copyright © 2017 Mockin_Aeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SleepCycleAlarm/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SleepCycleAlarm
+//
+//  Created by Mockin_Aeon. on 4/13/17.
+//  Copyright © 2017 Mockin_Aeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
